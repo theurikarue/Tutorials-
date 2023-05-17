10283ad8e1e431a27579177613ae088762399040
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-05-17 21:01:31 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,24 @@
+#Here is a short tutorial to help you jumpstart your journey on numbers 
+
+
+print (10 % 3) #returns remainder of 10 divided by 3
+
+print (10 % 2) #returns remainder of 10 divided by 2
+my_num=-7
+print(str(my_num) + " is my favourite number")
+print(abs(my_num)) #returns absolute value of my_num
+print(pow(my_num,2)) #raises my_num to the power of two
+print(max(my_num,200)) #returns the larger value of the two 
+print(min(my_num,200))
+
+#heres a library of some sort for python for math that one can import this is how it is done
+#syntax
+from math import * #this enables us to use get more functions
+#eg
+print(floor(6.789)) #it rounds down to nearest whole number
+print(ceil(6.789)) #fn that does opp of floor
+print(round(6.789)) #fn that rounds off a number
+print(sqrt(900)) #fn that gets square root of a number
+
+
+
