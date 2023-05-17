2a4d47a6713e29fbbd2e4eea218a25026fba414d
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-05-17 21:02:34 +0300

    Add files via upload

---
+++ string manipulation.py
@@ -0,0 +1,18 @@
+string =("My name is Bonface Karue")
+print(string.upper()) #in upper case
+print(string.lower()) #in lower case
+print(string.isupper()) #to check whether it is in upper case
+print(string.islower()) #to check whether it is in lower case
+print(len(string)) # to check the length of the string
+print(string[0])# returns character in first position of the string
+vowels="aeiouAEIOU"
+#print(string.count(vowels))
+#vowels = 'AEIOUaeiou'
+#for ele in vowels:
+
+#	string=string.replace(ele, K)
+#	K="*"
+print(string.index("a")) #returns index of first instance of letter a 
+print(string.index("face")) #returns index of where face begins
+print(string.replace("face","ke").replace("Karue","Reinhard")) #replaces face with ke
+print(string.split())
