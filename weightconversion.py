85772f466855419a30c4bbcb3ba85679deaa448b
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-06-20 03:13:51 -0700

    Update weightconversion.py
    added code

--- weightconversion.py
+++ weightconversion.py
@@ -1,4 +1,14 @@
-weight=input("weight: ")
-unit=print("(k)Kgs or (l)Lbs")
+weight = float(input("Weight: "))
+unit = input("(k) Kgs or (l) Lbs: ")
+
+if unit.lower() == 'k':
+    converted_weight = weight * 2.20462
+    print("Converted weight:", converted_weight, "Lbs")
+elif unit.lower() == 'l':
+    converted_weight = weight / 2.20462
+    print("Converted weight:", converted_weight, "Kgs")
+else:
+    print("Invalid unit! Please choose either 'k' or 'l'.")
+
 
     
